4850c50028535f955bad493008a722df250d5f84
Volodymyr <syvochka64@gmail.com>
2021-01-25 20:05:21 +0100

    module exports

---
+++ server/controllers/index.js
@@ -0,0 +1,13 @@
+const books = require('./books');
+const users = require('./users');
+const payments = require('./payments');
+const addresses = require('./addresses');
+const orders = require('./orders');
+
+module.exports = {
+  books,
+  users,
+  payments,
+  addresses,
+  orders
+};
