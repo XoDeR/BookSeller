1435ff852711489ae3c28ab9bf329c4d1e23a71b
Volodymyr <syvochka64@gmail.com>
2021-01-25 20:17:24 +0100

    added stripe and stripe private key

--- server/controllers/payments.js
+++ server/controllers/payments.js
@@ -1,4 +1,4 @@
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")(sk_test_51IDaPbEcQoHsXCCF6WcdCXGqsulPALNrwxyUASMMpKmtVdB510T5VYkqpIV8nSkBsKFBZA75ePFE8UR49XmQviWr00ChwSF0Ce);
 
 module.exports = {
   
