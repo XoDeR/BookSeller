2bf6fd8272789188b4f9bc924bea85a7f4ef68ae
Volodymyr <syvochka64@gmail.com>
2021-01-25 19:56:28 +0100

    addresses controller

---
+++ server/controllers/addresses.js
@@ -0,0 +1,65 @@
+const Address = require('../models').Address;
+
+module.exports = {
+  
+  create(req, res) {
+    return Address
+      .create({
+        addressLine: req.body.addressLine,
+        city:req.body.city,
+        state:req.body.state,
+        zip:req.body.zip,
+        country:req.body.country,
+        userId:req.params.userId
+      })
+      .then(address => res.status(201).send(address))
+      .catch(error => res.status(400).send(error));
+  },
+  
+  retrieve(req, res){
+    return Address
+    .findAll({
+        where:{
+            userId:req.params.userId
+        },
+    })
+    .then(address => {
+        if(!address){
+            return res.status(404).send({
+                message: 'Address not found',
+        })
+    }
+    return res.status(200).send(address);
+    })
+    .catch(error => res.status(400).send(error)); 
+  },
+  
+  list(req, res){
+    return Address
+    .all()
+    .then(address => res.status(200).send(address))
+    .catch(error => res.status(400).send(error));
+  },
+
+  destroy(req, res){
+      return Address
+      .find({
+          where:{
+              id:req.params.addressId,
+              userId:req.params.userId
+          },
+      })
+      .then(address => {
+          if(!address){
+              return res.status(404).send({
+                  message:'Address not found',
+              });
+          }
+          return address
+          .destroy()
+          .then(()=> res.status(204).send())
+          .catch(error => res.status(400).send(error));
+      })
+      .catch(error => res.status(400).send(error));
+  }
+};
