717e26b70f902e5e88b1dc11bd5fff446995ead1
Volodymyr <syvochka64@gmail.com>
2021-01-25 20:31:36 +0100

    added routes

--- app.js
+++ app.js
@@ -4,15 +4,17 @@ const bodyParser = require('body-parser');
 
 require('dotenv').config();
 
-// Set up the express app
 const app = express();
 
-// Log requests to the console.
 app.use(logger('dev'));
-
-// Parse incoming requests data (https://github.com/expressjs/body-parser)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// Routes
+const authRoutes = require('./server/routes/auth');
+const apiRoutes = require('./server/routes/index');
+app.use('/auth',authRoutes);
+app.use('/api',apiRoutes);
 
 // Setup a default catch-all route that sends back a welcome message in JSON format.
 app.get('*', (req, res) => res.status(200).send({
