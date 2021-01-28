4658a6d70fdf5c2de595b503267be5ecaa7e7767
Volodymyr <syvochka64@gmail.com>
2021-01-28 19:29:36 +0100

    passport; local-signup

---
+++ server/config/passport/local-signup.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+const User = require('../../models').User;
+const PassportLocalStrategy = require('passport-local').Strategy;
+
+module.exports = new PassportLocalStrategy({
+    usernameField:'email',
+    passwordField:'password',
+    session:false,
+    passReqToCallback:true
+  },
+
+  function(req, email, password, done) {
+    User.create({
+      firstName:req.body.firstName.trim(),
+      lastName:req.body.lastName.trim(),
+      email:email.toLowerCase().trim(),
+      password:password.trim()
+    })
+    .then(function(newUser){
+        if(!newUser){
+            return done(null,false,{message:'Incorrect Credentials'});
+        }
+        if (newUser){
+            return done(null,newUser);
+        }
+    })
+    .catch(function(err){ return done(err)});
+  }
+);
+
