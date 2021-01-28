577e4d150a24800adbc34f5e95edd26f41970c11
Volodymyr <syvochka64@gmail.com>
2021-01-28 19:59:40 +0100

    404

---
+++ client/src/pages/FourOhFour.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const FourOhFour = () => (
+  <div>
+    <h1>404</h1>
+    <p>The page you are looking for is not available</p>
+  </div>
+);
+
+export default FourOhFour;
