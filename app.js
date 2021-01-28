86a4868e71aefbff591eb109c7f5df88765c5a9e
Volodymyr <syvochka64@gmail.com>
2021-01-28 19:51:31 +0100

    server; app

--- app.js
+++ app.js
@@ -1,22 +1,35 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
-
+const path = require('path');
+const cors = require('cors');
+var passport = require('passport');
+var session = require('express-session');
 require('dotenv').config();
 
 const app = express();
-
+app.use(cors());
 app.use(logger('dev'));
+app.use(bodyParser.text());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+var models = require('./server/models');
+
+app.use(passport.initialize());
+const localSignupStrategy = require('./server/config/passport/local-signup');
+const localLoginStrategy = require('./server/config/passport/local-login');
+passport.use('local-signup', localSignupStrategy);
+passport.use('local-login',localLoginStrategy);
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 // Routes
-////const authRoutes = require('./server/routes/auth');
+const authRoutes = require('./server/routes/auth');
 const apiRoutes = require('./server/routes/index');
-////app.use('/auth',authRoutes);
+app.use('/auth',authRoutes);
 app.use('/api',apiRoutes);
 
-// Setup a default catch-all route that sends back a welcome message in JSON format.
 app.get('*', (req, res) => res.status(200).send({
   message: 'Welcome to the beginning of nothingness.',
 }));
